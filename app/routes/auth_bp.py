17e8bfeaaf41aa93ab7f1046ffdb1565d608d843
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2022-07-21 01:24:54 -0400

    CORS fix

--- app/routes/auth_bp.py
+++ app/routes/auth_bp.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask_cors import CORS, cross_origin
 from flask_jwt_extended import jwt_required
 from app.controllers.UserController import login, signup, logout, refresh, authorized
 
@@ -7,7 +8,7 @@ auth_bp = Blueprint('auth_bp', __name__)
 
 auth_bp.route('/login', methods = ['POST'])(login)
 auth_bp.route('/signup', methods = ['POST'])(signup)
-auth_bp.route('/refresh', methods = ['GET'])(jwt_required(refresh=True)(refresh))
+auth_bp.route('/refresh', methods = ['GET'])(cross_origin(allow_headers=['Content-Type','Authorization'])(jwt_required(refresh=True)(refresh)))
 auth_bp.route('/logout', methods=['POST'])(jwt_required(refresh=True)(logout))
 
 # Test Route

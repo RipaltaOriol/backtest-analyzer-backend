1a639fe5ebb80ba00418174155e1520ce25293ec
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2022-07-21 01:31:37 -0400

    CORS fix

--- app/routes/auth_bp.py
+++ app/routes/auth_bp.py
@@ -8,7 +8,7 @@ auth_bp = Blueprint('auth_bp', __name__)
 
 auth_bp.route('/login', methods = ['POST'])(login)
 auth_bp.route('/signup', methods = ['POST'])(signup)
-auth_bp.route('/refresh', methods = ['GET'])(cross_origin(allow_headers=['Content-Type','Authorization'])(jwt_required(refresh=True)(refresh)))
+auth_bp.route('/refresh', methods = ['GET'])(jwt_required(refresh=True)(refresh))
 auth_bp.route('/logout', methods=['POST'])(jwt_required(refresh=True)(logout))
 
 # Test Route

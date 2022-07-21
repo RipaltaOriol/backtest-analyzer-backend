ca84b4c1bde53f7f247cdcfcdc8cf5878e78dd47
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2022-07-20 21:41:15 -0400

    CORS fix

--- app/routes/auth_bp.py
+++ app/routes/auth_bp.py
@@ -6,7 +6,7 @@ from app.controllers.UserController import login, signup, logout, refresh, autho
 # Initialize blueprint
 auth_bp = Blueprint('auth_bp', __name__)
 
-auth_bp.route('/login', methods = ['POST', 'OPTIONS'])(cross_origin()(login))
+auth_bp.route('/login', methods = ['POST'])(cross_origin(automatic_options=True)(login))
 auth_bp.route('/signup', methods = ['POST'])(signup)
 auth_bp.route('/refresh', methods = ['GET'])(jwt_required(refresh=True)(refresh))
 auth_bp.route('/logout', methods=['POST'])(jwt_required(refresh=True)(logout))

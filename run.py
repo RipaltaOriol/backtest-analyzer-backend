e18c53aea8bc8417ae7b8818a5929f4b93ba6e6f
RipaltaOriol <48039207+RipaltaOriol@users.noreply.github.com>
2023-06-02 15:32:28 -0400

    Revert "Feat: Refactor PDF Export"

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
-from dotenv import load_dotenv
 
 if __name__ == "__main__":
     # app.secret_key = 'secret-backtest-analyzer'
-    debug = app.config["DEBUG"]
-
-    app.run(debug=debug)
+    app.run()

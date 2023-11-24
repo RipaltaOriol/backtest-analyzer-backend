778dd492067e56817cec137b32f52a4c20ea51ba
Uri's Mac <oriolripalta@hotmail.com>
2023-11-24 19:52:35 +0100

    remove routes test

--- tests/test_project.py
+++ tests/test_project.py
@@ -9,7 +9,7 @@ def test_utils():
     assert parse_column_name("col_m_test") == "test"
 
 
-def test_routes():
-    with app.test_client() as test_client:
-        response = test_client.get("/documents")
-        assert "hello" == "hello"
+# def test_routes():
+#     with app.test_client() as test_client:
+#         response = test_client.get("/documents")
+#         assert "hello" == "hello"

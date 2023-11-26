344185efdddc4d210c1e54948a143987e3b5a88d
Uri's Mac <oriolripalta@hotmail.com>
2023-11-26 13:27:42 +0100

    fix lint

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
 class ProductionConfig(Config):
     # Connect to the database
     DEBUG = False
-    MONGO_URI = "mongodb+srv://appadmin0:backtestanalyzer@clusterbeta.xm49b.mongodb.net/backtest-analyzer?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+    # MONGO_URI = "mongodb+srv://appadmin0:backtestanalyzer@clusterbeta.xm49b.mongodb.net/backtest-analyzer?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
     # JWT config
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     pass
@@ -26,7 +26,7 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     # Connect to the database
-    MONGO_URI = "mongodb://localhost:27017/backtest-analyzer"
+    # MONGO_URI = "mongodb://localhost:27017/backtest-analyzer"
     # JWT config
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     DEBUG = True
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,17 +1,11 @@
 import pytest
-from app.controllers.UploadController import upload_mt4
-from app.controllers.utils import from_db_to_df
-from pandas.testing import assert_frame_equal
 
 # from app import app
-# from app.controllers.utils import parse_column_name
+from app.controllers.utils import parse_column_name
 
 
 def test_utils():
-    # res = app.controllers.utils.parse_column_name("Hello")
-    # print(res)
-    # assert parse_column_name("col_m_test") == "test"
-    assert "test" == "test"
+    assert parse_column_name("col_m_test") == "test"
 
 
 # def test_routes():

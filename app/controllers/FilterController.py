c3f79bf6d3aeeffe7a905b1dfaf3b31f55ec2a89
Uri's Mac <oriolripalta@hotmail.com>
2024-02-11 16:38:37 +0100

    refactor: apply filters func

--- app/controllers/FilterController.py
+++ app/controllers/FilterController.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 from app import app
 from app.controllers.ErrorController import handle_403
-from app.controllers.SetupController import get_filter_options
 from app.controllers.utils import from_db_to_df, from_df_to_db, retrieve_filter_options
 from app.models.Document import Document
 from app.models.Filter import Filter
@@ -64,6 +63,53 @@ def apply_filter(df, column, operation, value):
             df = df[df[column] != value]
 
     return df
+
+
+def get_filter_options(doucment_id):
+    """
+    Gets Setup Filter Options
+    """
+    document = Document.objects(id=doucment_id).get()
+
+    data = from_db_to_df(document.state)
+    data.replace({np.nan: None}, inplace=True)
+
+    map_types = data.dtypes
+    options = []
+    for column in data.columns:
+        if column.startswith("col_m_"):
+            option = {"id": column, "name": column[6:]}
+            if map_types[column] == "float64" or map_types[column] == "int64":
+                option.update(type="number")
+            else:
+                option.update(type="string")
+                option.update(values=list(data[column].dropna().unique()))
+            options.append(option)
+        if column.startswith("col_p"):
+            option = {
+                "id": column,
+                "name": "Pair",
+                "type": "string",
+                "values": list(data[column].dropna().unique()),
+            }
+            options.append(option)
+        if column.startswith("col_rr"):
+            option = {
+                "id": column,
+                "name": "Risk Reward",
+                "type": "number",
+                "values": list(data[column].dropna().unique()),
+            }
+            options.append(option)
+        if column.startswith("col_d_"):
+            option = {
+                "id": column,
+                "name": column[6:],
+                "type": "date",
+            }
+            options.append(option)
+
+    return options
 
 
 def get_filter_name(column, operation, value):

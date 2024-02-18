9c02a732bb5f43eb6a8e8b229cbb2c9af0bc23bb
Uri's Mac <oriolripalta@hotmail.com>
2024-02-18 17:22:34 +0100

    feat: add direction to filters

--- tests/controllers/filters/test_apply_filter.py
+++ tests/controllers/filters/test_apply_filter.py
@@ -2,6 +2,8 @@ from app.controllers.FilterController import apply_filter
 from pandas.testing import assert_frame_equal
 from tests.controllers.filters.test_apply_filter_data import (
     filtered_data_date,
+    filtered_data_direction_exclude,
+    filtered_data_direction_include,
     unfiltered_data,
 )
 
@@ -11,3 +13,18 @@ def test_apply_filters_date():
         unfiltered_data, "date", "date", ["05/01/2018", "05/30/2018"]
     )
     assert_frame_equal(filtered_df, filtered_data_date)
+
+
+def test_apply_filters_direction_include():
+    filtered_df = apply_filter(unfiltered_data, "col_d", "in", ["long"])
+    assert_frame_equal(filtered_df, filtered_data_direction_include)
+
+
+def test_apply_filters_direction_exclude():
+    filtered_df = apply_filter(unfiltered_data, "col_d", "nin", ["LONG"])
+    assert_frame_equal(filtered_df, filtered_data_direction_exclude)
+
+
+def test_apply_filters_direction_all():
+    filtered_df = apply_filter(unfiltered_data, "col_d", "in", ["Long", "Short"])
+    assert_frame_equal(filtered_df, unfiltered_data)
